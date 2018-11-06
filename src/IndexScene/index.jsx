19e5f92b371765cd4e7ee45b9227932c6015c98b
luker <lukerlexa@gmail.com>
2018-11-06 11:53:51 +0200

    initial commit

---
+++ src/IndexScene/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class IndexScene extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+
+    return (
+      <div>
+        div
+      </div>
+    )
+
+  }
+}
+
+IndexScene.propTypes = {}
+
+export default IndexScene
